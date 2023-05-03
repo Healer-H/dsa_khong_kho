2481e0fa369dc9903a047c051186ae2fa8549bbd
Hieu Minh HuynH <22520434@gm.uit.edu.vn>
2023-05-03 20:28:55 +0700

    add template

---
+++ competitive_programming/template_competitive_programming.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int const MAXN = 1e6 + 7;
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define REPD(i, b, a) for(int i = b; i >= a; --i)
+#define sz(s) (int)(s).size()
+
+void solve()
+{
+     
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int numTest = 1;
+    cin >> numTest;
+    while(numTest--)
+    {
+        solve();
+    }
+}
